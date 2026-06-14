4e164e2e2a17b97da7c803b2cc035f8be92526e5
falltrades <thaikhuevincent.nguyen@gmail.com>
2026-06-14 11:05:16 +0200

    feat(auth): add --dart-define support for Google Web Client ID

--- lib/services/google_sign_in_singleton.dart
+++ lib/services/google_sign_in_singleton.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSignInSingleton {
@@ -6,6 +7,10 @@ class GoogleSignInSingleton {
   static GoogleSignIn get instance => GoogleSignIn.instance;
 
   static Future<void> initialize() async {
-    await instance.initialize();
+    const webClientId = String.fromEnvironment('GOOGLE_WEB_CLIENT_ID');
+
+    await instance.initialize(
+      clientId: kIsWeb ? webClientId : null,
+    );
   }
 }
